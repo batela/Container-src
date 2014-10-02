1345da866ebc490a322dd3d980e112e6aa24cd56
Borja Tec <batela@batelasoftware.es>
2014-10-02 07:08:45 +0200

    Añado clase calle para la localización de containers

---
+++ libsaioa/include/Calle.h
@@ -0,0 +1,17 @@
+/*
+ * Calle.h
+ *
+ *  Created on: Oct 2, 2014
+ *      Author: borja
+ */
+
+#ifndef CALLE_H_
+#define CALLE_H_
+
+class Calle {
+public:
+	Calle();
+	virtual ~Calle();
+};
+
+#endif /* CALLE_H_ */
---
+++ libsaioa/src/Calle.cpp
@@ -0,0 +1,18 @@
+/*
+ * Calle.cpp
+ *
+ *  Created on: Oct 2, 2014
+ *      Author: borja
+ */
+
+#include "Calle.h"
+
+Calle::Calle() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Calle::~Calle() {
+	// TODO Auto-generated destructor stub
+}
+
