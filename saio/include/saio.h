fc65275e97be69e2cd690f64134df9494a7c9ea5
Borja <batela@batelasoftware.es>
2014-10-02 19:30:14 +0200

    Creamos fichero de cabecera

---
+++ saio/include/saio.h
@@ -0,0 +1,15 @@
+#ifndef LL2XY_H_
+#define LL2XY_H_
+
+#include "log4cpp/Category.hh"
+#include "log4cpp/Appender.hh"
+#include "log4cpp/OstreamAppender.hh"
+#include "log4cpp/FileAppender.hh"
+#include "log4cpp/Layout.hh"
+#include "log4cpp/BasicLayout.hh"
+#include "log4cpp/Priority.hh"
+#include "log4cpp/NDC.hh"
+#include "log4cpp/PropertyConfigurator.hh"
+
+
+#endif
